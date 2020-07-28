61eb249c85028f4010e18dc9e8f7418f2ce526f7
Lucas <lucasachet@gmail.com>
2020-07-28 11:56:07 -0300

    Challenge 4: Change the Color of All Buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,7 +116,7 @@ function reset3() {
   imgScissors.height = "150";
   imgScissors.width = "150";
   imgScissors.onclick = function () { rpsGame(this) };
-  
+
   originalHtml.appendChild(imgRock);
   originalHtml.appendChild(imgPaper);
   originalHtml.appendChild(imgScissors);
@@ -127,4 +127,57 @@ function reset3() {
   btnDiv.appendChild(resetbtn)
   newDiv.appendChild(originalHtml)
   newDiv.appendChild(btnDiv)
+}
+
+
+//Challenge 4: Change the Color of All Buttons
+var allButtons = document.getElementsByTagName('button');
+
+var copyAllButtons = [];
+for (let i = 0; i < allButtons.length; i++) {
+  copyAllButtons.push(allButtons[i].classList[1])
+}
+
+
+
+function buttonColorChange(onChangeButton) {
+  if (onChangeButton.value === 'red') {
+    buttonsRed();
+  } else if (onChangeButton.value === 'green') {
+    buttonsGreen();
+  } else if (onChangeButton.value === 'reset') {
+    buttonColorReset();
+  } else if (onChangeButton.value === 'random') {
+    randomColors();
+  }
+}
+
+function buttonsRed() {
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].classList.remove(allButtons[i].classList[1]);
+    allButtons[i].classList.add('btn-danger');
+  }
+}
+function buttonsGreen() {
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].classList.remove(allButtons[i].classList[1]);
+    allButtons[i].classList.add('btn-success');
+  }
+}
+
+function buttonColorReset() {
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].classList.remove(allButtons[i].classList[1]);
+    allButtons[i].classList.add(copyAllButtons[i])
+  }
+}
+
+function randomColors() {
+  let choices = ['btn-danger', 'btn-warning', 'btn-success', 'btn-primary'];
+
+  for (let i = 0; i < allButtons.length; i++) {
+    let randomNumber = Math.floor(Math.random() * 4);
+    allButtons[i].classList.remove(allButtons[i].classList[1]);
+    allButtons[i].classList.add(choices[randomNumber]);
+  }
 }
